1e76900063ba69f7aa51f76009a82d9c740a0f36
dieum512 <akonkwadieum222@gmail.com>
2023-04-01 08:29:15 +0200

    Update the calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Buttons from './Buttons';
+import calculate from '../logic/calculate';
+
+const Calculs = () => {
+  const [result, updateResult] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (event) => {
+    updateResult(calculate(result, event.target.textContent));
+  };
+
+  return (
+    <div className="container">
+      <form className="calculs">
+        <input className="calculs" type="text" value={result.total || result.next || '0'} readOnly/>
+      </form>
+      <Buttons handleClick={handleClick} />
+    </div>
+
+  );
+};
+
+export default Calculs;
