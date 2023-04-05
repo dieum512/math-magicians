f57ca1e15cfe65dadd20ba54fa2925718d9ce718
dieum512 <akonkwadieum222@gmail.com>
2023-04-05 17:33:14 +0200

    Create the Home page

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="div-container">
+      <h2>Welcome to our page</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur ea molestiae optio nulla reprehenderit accusamus vitae magnam enim aut a vel sit necessitatibus aspernatur dolore, tempora labore odio eaque alias?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur ea molestiae optio nulla reprehenderit accusamus vitae magnam enim aut a vel sit necessitatibus aspernatur dolore, tempora labore odio eaque alias?</p>
+    </div>
+  );
+};
+
+export default Home;
