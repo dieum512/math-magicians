75566bb0c8e4ad31df57c82c8c2336bd10c78a85
dieum512 <akonkwadieum222@gmail.com>
2023-04-05 22:01:28 +0200

    Use the Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Quotes from './components/Quotes';
 import Calculs from './components/Calculator';
+import Home from './components/Home';
+import Nav from "./components/Nav";
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <>
-    <Quotes />
-    <Calculs />
+    <Nav />
+    <div className='all-container'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/calculator' element={<Calculs />} />
+        <Route path='/quotes' element={<Quotes />} />
+      </Routes>
+    </div>
+    {/* <Home /> */}
+    {/* <Calculs /> */}
+    {/* <Quotes /> */}
   </>
 );
 
